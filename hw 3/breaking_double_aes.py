e24c1262d30abdbeb05aca405a17fd5d52f8fffe
Igor <bboyifeel@gmail.com>
2020-04-15 22:02:30 +0200

    optimized cracking double aes

--- hw 3/breaking_double_aes.py
+++ hw 3/breaking_double_aes.py
@@ -1,47 +1,54 @@
 from Crypto.Cipher import AES
 
-def toHexString(string):
-	return ''.join(x.encode('hex') for x in string)
+def encrypt (key, ptext):
+	return AES.new(key, AES.MODE_ECB).encrypt(ptext)
 
-plaintext = "cb7488bac2092786d915c713a05c5bcf".decode("hex")
-ciphertext = "9246351a1af86adc5be32acdd45be2e4".decode("hex")
+def decrypt(key, ctext):
+	return AES.new(key, AES.MODE_ECB).decrypt(ctext)
 
-dictionary = dict()
+def check(plaintext, ciphertext, key1, key2):
+	if encrypt(key2, encrypt(key1, plaintext)) == ciphertext:
+		print('Keys are correct!')
 
-keyRange = 1<<24
+def crack():
+	plaintext = 0xcb7488bac2092786d915c713a05c5bcf.to_bytes(16, byteorder='big')
+	ciphertext = 0x9246351a1af86adc5be32acdd45be2e4.to_bytes(16, byteorder='big')
 
-print "[start] build forward dictionary"
+	dictionary = dict()
 
-for x in range(keyRange):
-	key = "{0:06x}".format(x) + "00" * 13
-	aes = AES.new(key.decode("hex"), AES.MODE_ECB)
-	intermediate = toHexString(aes.encrypt(plaintext))
-	dictionary[intermediate] = key
+	keyRange = 1<<24
 
-print "[done] build forward dictionary"
+	print ("[start] build forward dictionary")
+	for x in range(keyRange):
+		x <<= 104
+		key = x.to_bytes(16, byteorder='big')
+		intermediate = int.from_bytes(encrypt(key, plaintext), byteorder='big')
+		dictionary[intermediate] = key
+	
+	print ("[done] build forward dictionary")
 
-print "[start] bruteforce"
+	print ("[start] bruteforce")
 
-for x in range(keyRange):
-	key = "{0:06x}".format(x) + "00" * 13
-	aes = AES.new(key.decode("hex"), AES.MODE_ECB)
+	for x in range(keyRange):
+		x <<= 104
+		key = x.to_bytes(16, byteorder='big')
+		intermediate = int.from_bytes(AES.new(key, AES.MODE_ECB).decrypt(ciphertext), byteorder='big')
 
-	intermediate = toHexString(aes.decrypt(ciphertext))
+		if intermediate in dictionary:
+			key1 = dictionary[intermediate]
+			key2 = key
+	
+			check(plaintext, ciphertext, key1, key2)
+			
+			key1 = int.from_bytes(key1, byteorder='big')
+			key2 = int.from_bytes(key2, byteorder='big')
+			print ("key 1: {}\nkey 2: {}".format(key1, key2))
+			print ("IN HEX:")
+			print ("key 1: {}\nkey 2: {}".format(hex(key1), hex(key2)))
+			break
 
-	if intermediate in dictionary:
-		print "key 1: {} \nkey 2: {}".format(dictionary[intermediate], key)
-		break
+	print ("[done] bruteforce")
 
-print "[done] bruteforce"
 
-# plaintext = "cb7488bac2092786d915c713a05c5bcf".decode("hex")
-
-# key_1 = "fe355a00000000000000000000000000".decode("hex") 
-# key_2 = "e5d97f00000000000000000000000000".decode("hex")
-
-# aes = AES.new(key_1, AES.MODE_ECB)
-# intermidiate = aes.encrypt(plaintext)
-# aes = AES.new(key_2, AES.MODE_ECB)
-# ciphertext = toHexString(aes.encrypt(intermidiate))
-
-# print ciphertext
+if __name__ == "__main__":
+	crack()
