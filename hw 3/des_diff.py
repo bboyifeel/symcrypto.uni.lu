164d55e80e9d7855a62bb5dcdc9286659118d0e0
Igor <bboyifeel@gmail.com>
2020-04-29 11:41:27 +0200

    *

--- hw 3/des_diff.py
+++ hw 3/des_diff.py
@@ -1,3 +1,4 @@
+"""subtask 1"""
 x = 0x25
 delta_in = 0x01
 delta_out = 0x06
@@ -17,8 +18,60 @@ def fi(x, k):
 
 	return S[r][y]
 
-if __name__ == "__main__":
+def subtask_1():
 	kmax = 1 << 6
 	for k in range(kmax):
 		if (fi(x,k) ^ fi(x^delta_in, k)) == delta_out:
 			print(hex(k))
+
+"""subtask 2"""
+
+def calc_deltas(ciphertext_diff):
+	delta_1 = 0x04000000
+	delta_2 = 0x405c0000
+
+	delta_6 = ciphertext_diff >> 32
+	delta_4 = ciphertext_diff & ((1 << 32) - 1)
+
+	delta_5 = delta_6 ^ delta_2
+	delta_3 = delta_4 ^ delta_1
+
+	print("delta_1: " + str(hex(delta_1)))
+	print("delta_2: " + str(hex(delta_2)))
+	print("delta_3: " + str(hex(delta_3)))
+	print("delta_4: " + str(hex(delta_4)))
+	print("delta_5: " + str(hex(delta_5)))
+	print("delta_6: " + str(hex(delta_6)))
+
+	print("[f] delta_2 to delta_3: " + str(hex(delta_2)) + " -> " + str(hex(delta_3)))
+	print("[f] delta_4 to delta_5: " + str(hex(delta_4)) + " -> " + str(hex(delta_5)))
+
+
+def subtask_2():
+	p1 = 0xf136991ac246bdc7
+	c1 = 0x953f1ad872392f41
+
+	p1star = 0xb16a991ac646bdc7
+	c1star = 0x2bec91cdf678be07
+
+	# print(hex(p1^p1star))
+	# print(hex(c1^c1star))
+
+	print("deltas for c1 and c1*")
+	calc_deltas(c1^c1star)
+
+	p2 = 0xe37ab19adfaddc95
+	c2 = 0xf7489e506f3f9529
+
+	p2star = 0xa326b19adbaddc95
+	c2star = 0xcfa1d97cebff856b
+
+	# print(hex(p2^p2star))
+	# print(hex(c2^c2star))
+
+	print("deltas for c2 and c2*")
+	calc_deltas(c2^c2star)
+
+
+if __name__ == "__main__":
+	subtask_2()
